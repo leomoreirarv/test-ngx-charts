6f39be7fc6d06ec2d3e05ea053392f8f48fceac7
fabmelo <fabmelo@gmail.com>
2019-01-16 18:21:52 -0200

    Reconstrução da estrutura de teste.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,103 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testngxcharts';
+
+  view: any[] = []; // define o tamanho do gráfico, ficando zerado ele ajusta
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Number';
+  showYAxisLabel = true;
+  yAxisLabel = 'Color Value';
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  multi: any[] = [
+    {
+      name: 'Cyan',
+      series: [
+        {
+          name: 5,
+          value: 2650,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 10,
+          value: 2800,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 15,
+          value: 2000,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 20,
+          value: 2999,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        }
+      ]
+    },
+    {
+      name: 'Yellow',
+      series: [
+        {
+          name: 5,
+          value: 2500,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 10,
+          value: 3100,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 15,
+          value: 2350,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        },
+        {
+          name: 20,
+          value: 2730,
+          extra: {
+            'value1': 'Hello',
+            'value2': 'World'
+          }
+        }
+      ]
+    }
+  ];
+
+  onSelect(e) {
+    console.log('=> ', e);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
